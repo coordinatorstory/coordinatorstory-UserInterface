dbb028198abc4493d25c896a2483f3df794ce252
azinoVo <azinovo22@gmail.com>
2019-04-17 21:34:47 -0500

    fin html/css/MQ for mvp, added JS

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,9 +1,15 @@
 //Javascript File for Landing and Abous Us Page
 // Classes must be built at the top!
 
-
-
-
-
-let nav = document.querySelector('.home');
-console.log(nav);
+let links = document.querySelectorAll('a');
+console.log(links);
+links.forEach(function(links){
+    links.addEventListener("mouseover",function(links){
+        links.target.style.color = "gold";
+    });
+});
+links.forEach(function(links){
+    links.addEventListener("mouseout",function(links){
+        links.target.style.color = "";
+    });
+});
